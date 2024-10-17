aa8f7d97491dd5c34de55e1083abe856c86a5088
antony496 <t691411@gmail.com>
2024-10-17 11:20:57 -0300

    jsmobile

---
+++ jsmobile/if.else/ifelse.js
@@ -0,0 +1,15 @@
+const idade = prompt("qual a sua cor favorita?")
+
+if (cor == "vermelho") {
+  console.log("vermelho");
+}
+
+else if (cor == "azul") {
+  console.log("azul");
+}
+
+else {
+  console.log("não conheço essa cor");
+}
+
+console.log(cor.length)
